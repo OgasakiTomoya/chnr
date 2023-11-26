a81e31fe97d374b787f4b1b15f4768f102abbebd
OgasakiTomoya <OgasakiTomoya@users.noreply.github.com>
2023-11-26 22:35:42 +0800

    Update ros-dpbr.sh

--- ros-dpbr.sh
+++ ros-dpbr.sh
@@ -3,16 +3,19 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/soffchen/GeoIP2-CN/release/CN-ip-cidr.txt
+#wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/soffchen/GeoIP2-CN/release/CN-ip-cidr.txt
+
+# 17mon-CN
+wget --no-check-certificate -c -O 17mon-CN.txt https://github.com/17mon/china_ip_list/blob/master/china_ip_list.txt
 
 {
 echo "/ip firewall address-list"
 
-for net in $(cat CN.txt) ; do
-  echo "add list=CN address=$net comment=AS4809"
+for net in $(cat 17mon-CN.txt) ; do
+  echo "add list=17mon-CN address=$net comment=17mon-CN"
 done
 
-} > ../CN.rsc
+} > ../17mon-CN.rsc
 
 cd ..
 rm -rf ./pbr
